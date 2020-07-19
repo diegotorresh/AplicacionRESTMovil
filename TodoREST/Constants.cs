0c59bebc1516a48c8772ffc3be8caab9f945fe9c
Diego Torres <diego.torres.h@tecsup.edu.pe>
2020-07-19 15:20:45 -0500

    first commit

---
+++ TodoREST/Constants.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace TodoREST
+{
+    public static class Constants
+    {
+        // URL of REST service
+       
+        public static string RestUrl = "http://100.25.221.36:3000/usuarios/listar";
+        public static string RestUrld = "http://100.25.221.36:3000/usuarios/{0}";
+        public static string RestUrli = "http://100.25.221.36:3000/usuarios/{0}";
+    }
+}
---
+++ TodoREST/Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TodoREST
+{
+	public class TodoItem
+	{
+		public long id { get; set; }
+
+		public string nombre { get; set; }
+
+		public string apellido { get; set; }
+		
+		public string sexo { get; set; }
+		public DateTime fecha { get; set; }
+	}
+}
